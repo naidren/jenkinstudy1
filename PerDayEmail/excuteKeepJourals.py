764f100a8f8766d03ec8d2b123897ed09b4d195d
naidren <1715182901@qq.com>
2023-02-14 10:15:03 +0800

    修改导入

--- PerDayEmail/excuteKeepJourals.py
+++ PerDayEmail/excuteKeepJourals.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # 导入包
-from DailyWork.recordWorkReport import MyReportOperate
+# from DailyWork.recordWorkReport import MyReportOperate
 from DailyWork.keepJournals import *
 import sys
 
