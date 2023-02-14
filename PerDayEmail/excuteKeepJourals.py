b7fd1a711cbc2658ac060e8d4d1c23bbb2cc6f60
naidren <1715182901@qq.com>
2023-02-14 10:16:57 +0800

    删除导入的包

--- PerDayEmail/excuteKeepJourals.py
+++ PerDayEmail/excuteKeepJourals.py
@@ -3,9 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # 导入包
-# from DailyWork.recordWorkReport import MyReportOperate
+
 from DailyWork.keepJournals import *
-import sys
 
 
 if __name__ == '__main__':
